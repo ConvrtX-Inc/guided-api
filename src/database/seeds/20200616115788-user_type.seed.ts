2931765be01acea9dde619bc59b7a662a1c71393
Marg <margarita@convrtx.com>
2022-02-16 18:43:32 +0800

    Update "Guide" to "Tourist Guide" and "Tourist" to "Traveller"

--- src/database/seeds/20200616115788-user_type.seed.ts
+++ src/database/seeds/20200616115788-user_type.seed.ts
@@ -11,12 +11,29 @@ export default class CreateUserType implements Seeder {
       .where('"UserType"."name" = :name', { name: 'Guide' })
       .getCount();
 
-    if (countGuide === 0) {
+    //update to Tourist Guide
+    if (countGuide > 0) { 
+      await connection
+        .createQueryBuilder()
+        .update(UserType)
+        .set({ name: 'Tourist Guide' })
+        .where([{ name: 'Guide' }])
+        .execute();
+    }
+
+    const countTouristGuide = await connection
+      .createQueryBuilder()
+      .select()
+      .from(UserType, 'UserType')
+      .where('"UserType"."name" = :name', { name: 'Tourist Guide' })
+      .getCount();      
+
+    if (countTouristGuide === 0) {
       await connection
         .createQueryBuilder()
         .insert()
         .into(UserType)
-        .values([{ name: 'Guide' }])
+        .values([{ name: 'Tourist Guide' }])
         .execute();
     }
     const countAdmin = await connection
@@ -28,7 +45,7 @@ export default class CreateUserType implements Seeder {
 
     if (countAdmin === 0) {
       await connection
-        .createQueryBuilder()
+        .createQueryBuilder() 
         .insert()
         .into(UserType)
         .values([{ name: 'Admin' }])
@@ -41,12 +58,29 @@ export default class CreateUserType implements Seeder {
       .where('"UserType"."name" = :name', { name: 'Tourist' })
       .getCount();
 
-    if (countTourist === 0) {
+    //update to Traveller
+    if (countTourist > 0) {          
+      await connection
+        .createQueryBuilder()
+        .update(UserType)
+        .set({ name: 'Traveller' })
+        .where([{ name: 'Tourist' }])
+        .execute();    
+    } 
+
+    const countTraveller = await connection
+      .createQueryBuilder()
+      .select()
+      .from(UserType, 'UserType')
+      .where('"UserType"."name" = :name', { name: 'Traveller' })
+      .getCount();
+
+    if (countTraveller === 0) {
       await connection
         .createQueryBuilder()
         .insert()
         .into(UserType)
-        .values([{ name: 'Tourist' }])
+        .values([{ name: 'Traveller' }])
         .execute();
     }
   }
