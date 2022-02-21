5e6cb476b5adce78648071b0e51152700751fdd1
Bernie Dalde <itdalde@yahoo.com>
2022-02-21 20:43:12 +0800

    Added convertion on image for advertisement image

--- src/activity-advertisement-image/entities/activity-advertisement-image.entity.ts
+++ src/activity-advertisement-image/entities/activity-advertisement-image.entity.ts
@@ -2,7 +2,17 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Allow, IsOptional, Validate } from "class-validator";
 import { EntityHelper } from "src/utils/entity-helper";
 import { IsExist } from "../../utils/validators/is-exists.validator";
-import { CreateDateColumn, UpdateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, Column, Generated } from "typeorm";
+import {
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    Generated,
+    BeforeUpdate, BeforeInsert, AfterLoad
+} from "typeorm";
+import {Transform} from "class-transformer";
 
 @Entity()
 export class ActivityAdvertisementImage extends EntityHelper {
@@ -11,21 +21,41 @@ export class ActivityAdvertisementImage extends EntityHelper {
 
     @IsOptional()
     @ApiProperty({ example: 'eae25276-3af3-432c-9c1b-7b7548513015' })
+    @Transform((value: string | null) => (value == '' ? null : value))
     @Validate(IsExist, ['ActivityAdvertisement', 'id'], {
         message: 'Activity advertisement not found',
     })
     @Column()
-    activity_advertisement_id: string;
+    activity_advertisement_id: string | null;
 
     @Allow()
     @IsOptional()
     @ApiProperty({ example: 'byte64image' })
+    @Transform((value: Buffer | null | string) => (value == null ? '' : value))
     @Column({
         name: 'snapshot_img',
         type: 'bytea',
-        nullable: false,
+        nullable: true,
     })
-    snapshot_img?: Buffer;
+    snapshot_img?: Buffer | null | string;
+    @BeforeUpdate()
+    @BeforeInsert()
+    public encodeImage() {
+        this.snapshot_img = this.snapshot_img
+          ? base64_arraybuffer.base64_2_ab(this.snapshot_img)
+          : '';
+    }
+
+    @AfterLoad()
+    public async decodeImage() {
+        try {
+            if (typeof this.snapshot_img !== null && this.snapshot_img != undefined) {
+                this.snapshot_img = await base64_arraybuffer.ab_2_base64(
+                  new Uint8Array(base64_arraybuffer.base64_2_ab(this.snapshot_img)),
+                );
+            }
+        } catch (e) {}
+    }
 
     @CreateDateColumn()
     created_date: Date;
