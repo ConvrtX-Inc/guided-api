d775ffc73b68c3618cab1dcb0bcb57ca7b6c9a2b
valerie-zenn <91924796+valerie-zenn@users.noreply.github.com>
2022-03-16 20:56:36 +0800

    Get the transactions by tour_guide_id and status

---
+++ src/transaction/dtos/transaction-user-status.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Allow } from 'class-validator';
+
+export class TransactionUserAndStatusDto {
+    @Allow()
+    @ApiProperty({ example: 'cbcfa8b8-3a25-4adb-a9c6-e325f0d0f3ae' })
+    tour_guide_id: string;
+
+    @Allow()
+    @ApiProperty({ example: 'completed' })
+    status: string;
+  }
