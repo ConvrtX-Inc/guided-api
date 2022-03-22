a688774597d267e1d60541f9a65b5923168c3b00
Dinalyn Permejo <dinalyn@convrtx.com>
2022-03-22 14:51:23 +0800

    Created/Reuse Payment Integration API - GUIDED-326

--- src/charge/charge.controller.ts
+++ src/charge/charge.controller.ts
@@ -10,6 +10,9 @@ import StripeService from '../stripe/stripe.service';
 import CreateChargeDto from './dtos/create-charge.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
+import { request } from 'http';
+import CreatePaymentDto from './dtos/create-payment.dto';
+import * as dateMath from 'date-arithmetic'
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
@@ -20,6 +23,7 @@ import { AuthGuard } from '@nestjs/passport';
 })
 export default class ChargeController {
   constructor(private readonly stripeService: StripeService) {}
+  private pricePerDay = 120;
 
   @Post()
   async createCharge(@Request() request, @Body() charge: CreateChargeDto) {
@@ -28,5 +32,14 @@ export default class ChargeController {
       charge.payment_method_id,
       request.user.stripe_customer_id,
     );
+  }
+
+  @Post('computePaymentPerDay')
+  async createPayment(@Request() request, @Body() amount: CreatePaymentDto){
+    const dateEnd = new Date(amount.end_date);
+    const dateStart = new Date(amount.start_date);
+    const days = dateMath.diff(dateStart, dateEnd, 'day', true);
+    const totalPayment = days * this.pricePerDay;
+    return totalPayment;
   }
 }
