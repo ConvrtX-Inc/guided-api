2fa6d9873a49b31ea099a36fcf4ffd72454b554b
valerie-zenn <91924796+valerie-zenn@users.noreply.github.com>
2022-03-30 10:06:36 +0800

    add import for status

--- src/activity-event/activity-event.module.ts
+++ src/activity-event/activity-event.module.ts
@@ -3,11 +3,15 @@ import { ActivityEventController } from './activity-event.controller';
 import { ActivityEventService } from './activity-event.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityEvent } from './activity-event.entity';
+import { StatusModule } from 'src/statuses/status.module';
 
 @Module({
   controllers: [ActivityEventController],
   providers: [ActivityEventService],
-  imports: [TypeOrmModule.forFeature([ActivityEvent])],
+  imports: [
+    StatusModule,
+    TypeOrmModule.forFeature([ActivityEvent])
+  ],
   exports: [ActivityEventService]
 })
-export class ActivityEventModule {}
+export class ActivityEventModule { }
