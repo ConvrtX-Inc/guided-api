b934cae2ca5ac3fd0dca8fe50313e8852f0eb7ac
valeriezenn <valerie@convrtx.com>
2022-04-24 23:19:48 +0800

    add badge and destination information

--- src/activity-package/activity-package.controller.ts
+++ src/activity-package/activity-package.controller.ts
@@ -12,10 +12,13 @@ import {
 import { ActivityPackageService } from './activity-package.service';
 import { ApiBearerAuth, ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { Crud, CrudController, Override } from '@nestjsx/crud';
+import { Crud, CrudController, CrudRequest, Override, ParsedRequest } from '@nestjsx/crud';
 import { ActivityPackage } from './activity-package.entity';
 import { UserProfileQuestionDto } from '../user-profile-question/dtos/user-profile-question.dto';
 import { ClosestActivityDto } from './dtos/activity-package.dto';
+import { getRepository } from 'typeorm';
+import { Badge } from 'src/badge/badge.entity';
+import { ActivityPackageDestination } from 'src/activity-package-destination/activity-package-destination.entity';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
@@ -46,7 +49,7 @@ import { ClosestActivityDto } from './dtos/activity-package.dto';
 export class ActivityPackageController
   implements CrudController<ActivityPackage>
 {
-  constructor(public service: ActivityPackageService) {}
+  constructor(public service: ActivityPackageService) { }
 
   get base(): CrudController<ActivityPackage> {
     return this;
@@ -55,6 +58,46 @@ export class ActivityPackageController
   @Override()
   async deleteOne(@Request() request) {
     return this.service.softDelete(request.params.id);
+  }
+
+  @Override('getManyBase')
+  async getMany(@ParsedRequest() req: CrudRequest) {
+    const activityPackage = await this.service.getMany(req);
+    if (activityPackage instanceof Array) {
+      for (const i in activityPackage) {
+        const badge = await getRepository(Badge)
+          .createQueryBuilder('badge')
+          .where({ id: activityPackage[i].main_badge_id })
+          .getOne();
+        activityPackage[i]['main_badge'] = badge;
+
+        const activityPackageDestination = await getRepository(ActivityPackageDestination)
+          .createQueryBuilder('activitypackagedestination')
+          .where("activitypackagedestination.activity_package_id = '" + activityPackage[i].id + "'")
+          .getRawOne();
+        activityPackage[i]['activity_package_destination'] = activityPackageDestination;
+      }
+    }
+
+    return activityPackage;
+  }
+
+  @Override('getOneBase')
+  async getOne(@ParsedRequest() req: CrudRequest) {
+    const activityPackage = await this.service.getOne(req);
+    const badge = await getRepository(Badge)
+      .createQueryBuilder('badge')
+      .where({ id: activityPackage.main_badge_id })
+      .getOne();
+    activityPackage['main_badge'] = badge;
+
+    const activityPackageDestination = await getRepository(ActivityPackageDestination)
+      .createQueryBuilder('activitypackagedestination')
+      .where("activitypackagedestination.activity_package_id = '" + activityPackage.id + "'")
+      .getRawOne();
+    activityPackage['activity_package_destination'] = activityPackageDestination;
+
+    return activityPackage;
   }
 
   @ApiOperation({ summary: 'Get the activity packages of a user' })
