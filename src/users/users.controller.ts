5827e40db55cfd63eacd37f58048eb9227f2e21b
jhenrie-arb <jhenrie@arbcalls.com>
2022-05-06 19:35:39 +0800

    GUIDED-521 (API | Admin - Add a dashboard API for end users)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,6 +26,7 @@ import { UsersCrudService } from './users-crud.service';
 import { infinityPagination } from 'src/utils/infinity-pagination';
 import { getRepository } from 'typeorm';
 import { Badge } from 'src/badge/badge.entity';
+import { BookingRequest } from 'src/booking-request/booking-request.entity';
 
 @ApiBearerAuth()
 // @Roles(RoleEnum.admin)
@@ -101,6 +102,13 @@ export class UsersController implements CrudController<User> {
         .createQueryBuilder('badge')
         .where("badge.id = '" + users[i].badge_id + "'")
         .getRawOne();
+
+      const total_booking = await getRepository(BookingRequest)
+        .createQueryBuilder('booking_request')
+        .where("booking_request.user_id = '" + users[i].id + "'")
+        .getCount();
+
+      users[i]['total_booking'] = total_booking;
       users[i]['badge'] = badge;
     }
 
@@ -110,12 +118,17 @@ export class UsersController implements CrudController<User> {
   @Override('getOneBase')
   async getOne (@ParsedRequest() req: CrudRequest) {
     const users = await this.service.getOne(req);
-    console.log(users);
     const badge = await getRepository(Badge)
       .createQueryBuilder('badge')
       .where("badge.id = '" + users.badge_id + "'")
       .getRawOne();
 
+    const total_booking = await getRepository(BookingRequest)
+      .createQueryBuilder('booking_request')
+      .where("booking_request.user_id = '" + users.id + "'")
+      .getCount();
+
+    users['total_booking'] = total_booking;
     users['badge'] = badge;
     return users;
   }
