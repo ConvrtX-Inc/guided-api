667d0d3d8c93d3a5eea76ffa7bc126abce79266e
mae <ednamae@convrtx.com>
2022-05-07 18:28:00 +0800

    updated notification api | added fields on transaction payment

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -5,12 +5,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FindOptions } from '../utils/types/find-options.type';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { DeepPartial } from '../utils/types/deep-partial.type';
+import { BookingRequestService } from 'src/booking-request/booking-request.service';
 
 @Injectable()
 export class NotificationService extends TypeOrmCrudService<Notification> {
   constructor(
     @InjectRepository(Notification)
     private repository: Repository<Notification>,
+    private bookingRequestSvc: BookingRequestService
   ) {
     super(repository);
   }
@@ -43,9 +45,64 @@ export class NotificationService extends TypeOrmCrudService<Notification> {
 
   async getNotificationByUser(user_id: string) {
     return this.repository.find({
-      where:  {
+      where: {
         user_id: user_id,
       },
-    });    
+    });
+  }
+
+  async getNotifications(notifications) {
+    let notifs = await Promise.all(
+      notifications.map(async (n) => {
+        if (n.booking_request_id) {
+          const booking_request = await this.bookingRequestSvc.findOne({
+            where: { id: n.booking_request_id },
+          });
+
+          if (booking_request) {
+            n.booking_request = {
+              id: booking_request.id,
+              status: booking_request.status.status_name,
+              from_user: booking_request.from_user_id,
+              to_user: booking_request.user_id,
+              is_approved: booking_request.is_approved
+            };
+          }
+        }
+
+        return n;
+
+      })
+    );
+
+    ///Sort notifications
+    notifs = notifs.sort((a: any, b: any) => {
+
+      let date1 = new Date(a?.created_date)
+      let date2 = new Date(b?.created_date)
+      return date2.valueOf() - date1.valueOf();
+    });
+
+    return notifs;
+  }
+
+  async getTravelerNotifications(user_id: string, filter: string) {
+    let notifications: any = await this.repository.find({
+      where: {
+        to_user_id: user_id,
+      },
+    });
+
+    notifications = await this.getNotifications(notifications);
+
+    switch (filter.toLowerCase()) {
+      case 'all':
+        return notifications
+      case 'accepted':
+        return notifications.filter((n) => n.booking_request.is_approved);
+      case 'rejected':
+        return notifications.filter((n) => !n.booking_request.is_approved);
+    }
+
   }
 }
