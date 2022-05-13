bae2bff6da3dda1f6c5a55c9c717db1e23142235
valeriezenn <valerie@convrtx.com>
2022-05-13 16:06:13 +0800

    mobile request

--- src/activity-package/activity-package.service.ts
+++ src/activity-package/activity-package.service.ts
@@ -236,16 +236,22 @@ export class ActivityPackageService extends TypeOrmCrudService<ActivityPackage> 
         .where("id = '" + activityAvailability[i].activity_package_id + "'")
         .getOne();
 
-        const check = response.find(x => x.id == activityPackage.id);
-        if (!check) {
-          const badge = await getRepository(Badge)
+      const check = response.find(x => x.id == activityPackage.id);
+      if (!check) {
+        const badge = await getRepository(Badge)
           .createQueryBuilder('badge')
           .where({ id: activityPackage.main_badge_id })
           .getOne();
         activityPackage['main_badge'] = badge;
 
-          response.push(activityPackage);
-        }
+        const activityPackageDestination = await getRepository(ActivityPackageDestination)
+          .createQueryBuilder('activitypackagedestination')
+          .where({ activity_package_id: activityPackage.id })
+          .getOne();
+        activityPackage['activity_package_destination'] = activityPackageDestination;
+
+        response.push(activityPackage);
+      }
     }
 
     return response;
